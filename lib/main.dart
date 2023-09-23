481c16edae8c6b77b22baacbeef957ef7d59b57f
Ibrahim <iibbrraahhimm00@gmai;.com>
2023-09-23 17:27:21 +0300

    Hadeth screen/Hadeth details

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:islami_application/core/theme/application_theme.dart';
 import 'package:islami_application/layout/home_layout.dart';
+import 'package:islami_application/moduls/hadeth_screen/hadeth_details.dart';
 import 'package:islami_application/moduls/quran_screen/quran_details.dart';
 import 'package:islami_application/moduls/splash_screen/splash_screen.dart';
 
@@ -20,6 +21,7 @@ class MyApplication extends StatelessWidget {
         SplashScreen.routeName: (context) => SplashScreen(),
         HomeLayout.routeName: (context) => HomeLayout(),
         QuranDetails.routeName: (context) => QuranDetails(),
+        HadethDetails.routeName: (context) => HadethDetails(),
       },
       theme: ApplicationTheme.lightTheme,
       darkTheme: ApplicationTheme.darkTheme,
